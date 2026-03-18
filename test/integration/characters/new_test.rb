dcdb3f44a6c42a058758ecb9f92ce2a04e1b5afa
Maciej N. <magnax@gmail.com>
2026-03-18 20:51:03 +0100

    Convert character creating tests from rspec to minitest

--- test/integration/characters/new_test.rb
+++ test/integration/characters/new_test.rb
@@ -23,4 +23,27 @@ class CharactersNewTest < ActionDispatch::IntegrationTest
     assert_current_path new_character_path(locale: 'en')
     assert_selector 'h1', text: 'New character'
   end
+
+  test 'actually creating a character' do
+    visit list_url
+    assert_text "You don't have any characters"
+    click_on 'Create new character'
+
+    fill_in 'Name', with: 'Magnus'
+    select('Female', from: 'Gender')
+
+    click_on 'Create character'
+    assert_text 'New character successfully created'
+    assert_text 'Your characters (1)'
+  end
+
+  test 'cannot create more than 15 characters' do
+    create_list(:character, 15, { user: @user })
+
+    visit list_url
+    assert_text 'Your characters (15)'
+    assert_text 'You cannot create more characters'
+
+    assert_no_link 'Create new character'
+  end
 end
