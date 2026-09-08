ec3a90181d754055022786d2cc507a4d7e87c3cb
Maciej N. <magnax@gmail.com>
2026-09-08 08:59:40 +0200

    Fix recount function - allow integer values only, round days

--- app/javascript/recount.ts
+++ app/javascript/recount.ts
@@ -3,20 +3,30 @@ function recount(currentField: HTMLInputElement) {
     return;
   }
 
+  const isNumber = ((currentField.value.trim() !== "") && Number.isInteger(Number(currentField.value)))
+
   const ratio = Number(currentField.value) / Number(currentField.dataset['amount']);
   const elements = currentField.form.elements;
 
   for (let element of elements) {
-    if (element instanceof HTMLInputElement && !(element == currentField)) {
-      let amount = Number(element.dataset['amount']) * ratio;
+    if (
+      element instanceof HTMLInputElement &&
+      !(element == currentField) &&
+      !(element.type == 'submit')
+    ) {
+      if (isNumber) {
+        let amount = Number(element.dataset['amount']) * ratio;
 
-      if (element.id == 'days') {
-        var strAmount = Math.round(amount * 100) / 100;
+        if (element.id == 'days') {
+          var strAmount = Math.ceil(amount * 4) / 4;
+        } else {
+          var strAmount = Math.floor(amount);
+        }
+
+        element.value = String(strAmount);
       } else {
-        var strAmount = Math.floor(amount);
+        element.value = '';
       }
-
-      element.value = String(strAmount);
     }
   }
 };
